35bf2b79b552b250ede4a5650a2eef16c4920d5e
Grant Pierce <glpierce174@gmail.com>
2021-11-23 14:20:10 -0500

    commit tree 1170f73107270f1f4737344752b686568ee94f9a

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(array, string) {
+    let newArray = [];
+    newArray = array.filter(element => element.toUpperCase() === string.toUpperCase());
+    return newArray;
+}
+
+function fuzzyMatch(array, string) {
+    let newArray = [];
+    newArray = array.filter(element => element.slice(0, string.length) === string);
+    return newArray;
+}
+
+function matchName(array, string) {
+    let newArray = [];
+    newArray = array.filter(element => element.name === string);
+    return newArray;
+}
